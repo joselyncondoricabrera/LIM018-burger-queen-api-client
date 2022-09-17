561c6e81029bea216f016bc0e02b658eed24be9a
LisethLira <liseth.lira.123@gmail.com>
2022-09-16 20:01:44 -0500

    avance react router basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 import email from './imagen/email.png';
 import key from './imagen/key2.png';
 import './App.scss';
-
 /* function App() {
   return (
     <div className="App">
@@ -28,7 +33,7 @@ import './App.scss';
   );
 } */
 
-export function Login() {
+function Login() {
   return (
     <div className="Form-login">
       <h1>Iniciar Sesión</h1>
@@ -45,5 +50,57 @@ export function Login() {
     </div>
   );
 }
-export function prueba() {
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav className="Nav-bar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Login">Login</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2 className="Title">Home</h2>
+    </div>
+  );
+}
+
+// function About() {
+//   return (
+//     <div>
+//       <h2 className="Title">About</h2>
+//     </div>
+//   );
+// }
+
+function Users() {
+  return (
+    <div>
+      <h2 className="Title">Users</h2>
+    </div>
+  );
 }
