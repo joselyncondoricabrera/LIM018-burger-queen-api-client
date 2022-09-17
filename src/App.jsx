a7f0fbd0fe0e9428043bc1a66061790613e6c037
LisethLira <liseth.lira.123@gmail.com>
2022-09-17 11:33:09 -0500

    router de componentes login y home

--- src/App.jsx
+++ src/App.jsx
@@ -8,75 +8,37 @@ import {
 import email from './imagen/email.png';
 import key from './imagen/key2.png';
 import './App.scss';
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
 
 function Login() {
   return (
-    <div className="Form-login">
-      <h1>Iniciar Sesión</h1>
-      <div className="Form-input">
-        <img src={email} className="Icon-login" alt="logo" />
-        <input type="text" className="Input-login" placeholder="ingrese email" />
-      </div>
-      <div className="Form-input">
-        <img src={key} className="Icon-login" alt="logo" />
-        <input type="text" className="Input-login" placeholder="ingrese contraseña" />
+    <div className="Background-login">
+      <div className="Form-login">
+        <h1>Iniciar Sesión</h1>
+        <div className="Form-input">
+          <img src={email} className="Icon-login" alt="logo" />
+          <input type="text" className="Input-login" placeholder="ingrese email" />
+        </div>
+        <div className="Form-input">
+          <img src={key} className="Icon-login" alt="logo" />
+          <input type="text" className="Input-login" placeholder="ingrese contraseña" />
+        </div>
+
+        <button type="button"><Link to="/Login">Ingresar</Link></button>
       </div>
 
-      <button type="button"> Enviar </button>
     </div>
+
   );
 }
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav className="Nav-bar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/Login">Login</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/Login" element={<Login />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/Login" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
@@ -85,17 +47,10 @@ function Home() {
   return (
     <div>
       <h2 className="Title">Home</h2>
+      <button type="button"><Link to="/Login">Ingresar</Link></button>
     </div>
   );
 }
-
-// function About() {
-//   return (
-//     <div>
-//       <h2 className="Title">About</h2>
-//     </div>
-//   );
-// }
 
 function Users() {
   return (
