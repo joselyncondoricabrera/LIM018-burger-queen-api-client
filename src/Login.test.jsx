3f30eb3a91fd189018b6fc2d3a6300b43f70b074
LisethLira <liseth.lira.123@gmail.com>
2022-10-14 10:08:27 -0500

    componente orders css y html, pruebas en test

--- src/Login.test.jsx
+++ src/Login.test.jsx
@@ -1,30 +1,45 @@
 /** * @jest-environment jsdom */
 
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 // import { useNavigate } from 'react-router-dom';
 import fetch from 'node-fetch';
 import loginUsers from './Requests/LoginRequest';
-// import App from './App';
+import App from './App';
 
 // jest.mock('node-fetch');
 jest.mock('node-fetch', () => jest.fn());
 
-// describe('Componente Login', () => {
-//   it('Debería existir el botón con className "Button-login"', () => {
-//     render(<App />);
-//     expect(screen.getByRole('button', { className: 'Button-login' })).toBeInTheDocument();
-//   });
+describe('Componente Login', () => {
+  it('Debería existir el botón con className "Button-login"', () => {
+    render(<App />);
+    expect(screen.getByRole('button', { className: 'Button-login' })).toBeInTheDocument();
+  });
 
-//   it('Debería de ejecutar el evento onclick', () => {
-//     render(<App />);
-//     screen.debug();
-//     // const btnLogin = app.queryByText('Iniciar Sesión');
-//     // const requestUsers = jest.fn();
-//     fireEvent.click(screen.queryByTestId('btnLogin'));
-//     expect(requestUsers).toHaveBeenCalled();
-//   });
-// });
+  // it('Debería de ejecutar el evento onclick', () => {
+  //   render(<App />);
+  //   screen.debug();
+  //   // const btnLogin = app.queryByText('Iniciar Sesión');
+  //   // const requestUsers = jest.fn();
+
+  //   const setItemSpy = jest.spyOn(window.sessionStorage, 'setItem');
+  //   const email = 'mesero1@gmail.com';
+  //   const password = '123456';
+  //   const secret = '12345';
+  //   const tokenTest = {
+  //     token: secret,
+  //   };
+  //   const json = { token: '12345' };
+  //   const resp = { json, status: 200 };
+  //   fetch.mockResolvedValue(resp);
+  //   const response = loginUsers(email, password).then((res) => res.json);
+  //   fireEvent.click(screen.queryByTestId('btnLogin'));
+  //   expect(response).toEqual(tokenTest);
+  //   expect(setItemSpy).toBeHaveBeenCalledWith('token', response.token);
+
+  // expect().toHaveBeenCalledWith();
+  // }
+});
 //--------------------------------------------------------------------------------
 
 // test de la función loginUsers
@@ -32,7 +47,7 @@ describe('loginUsers', () => {
   it('debería de ser una función', () => {
     expect(typeof loginUsers).toBe('function');
   });
-  it('tiene que retornar un token', () => {
+  it('tiene que retornar un objeto json con el token', () => {
     const secret = '12345';
     const tokenTest = {
       token: secret,
